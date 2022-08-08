f26838ed797863945c07a02772ae16a4e23bc0a6
NatalliaPahosava <mulato4ka114@gmail.com>
2022-08-08 19:52:16 -0400

    add some changes

---
+++ employees.js
@@ -0,0 +1,36 @@
+// import db from './mongoConnect.js'
+
+// const employees = db.collection('employees')
+
+// export const addEmployee = async (req, res) => {
+//     if (req.body.firstName && req.body.lastName && req.body.position) {
+//         try{
+//         const newEmployee = await employees.insertOne(req.body)
+//         res.send(newEmployee)
+//         }catch(err){
+//             res.send('error adding employee to database')
+//             throw err
+//         }  
+//     } else {
+//         res.send('Request body needs to have a firtsName, lastName and position!!!')
+//     }
+// }
+
+
+import db from './mongoConnect.js'
+
+const employees = db.collection('employees')
+
+export const addEmployee = async (req, res) => {
+  if (req.body.firstName && req.body.lastName && req.body.position) {
+    try {
+      const newEmployee = await employees.insertOne(req.body)
+      res.send(newEmployee)
+    } catch (err) {
+      res.send('error adding employee to database')
+      throw err
+    }
+  } else {
+    res.send('REQUEST body needs to have a firstName, lastName and position field!')
+  }
+}
--- mongoConnect.js
+++ mongoConnect.js
@@ -1,10 +1,38 @@
 import { MongoClient } from 'mongodb'
-import { uri } from "./credentials.js"
+ 
+// import { uri } from ""
 
-// const uri = 'mongodb://localhost:27017'
+const uri = 'mongodb://localhost:27017'
 
 const client = new MongoClient(uri) // creating client mongo instance
 const db = client.db('bocacode-test') // connecting to db = bocacod-test
-const movies = db.collection('movie') // getting the movie collection
 
-export default movies
+export default db
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { MongoClient } from 'mongodb'
+// import { uri } from "./credentials.js"
+
+// // const uri = 'mongodb://localhost:27017'
+
+// const client = new MongoClient(uri) // creating client mongo instance
+// const db = client.db('bocacode-test') // connecting to db = bocacod-test
+// // const movies = db.collection('movie') // getting the movie collection
+
+// export default db
