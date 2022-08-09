de0a2fb521c50424bba92da1587cce5cd0a1827a
NatalliaPahosava <mulato4ka114@gmail.com>
2022-08-08 20:59:42 -0400

    forced uri credentials

--- mongoConnect.js
+++ mongoConnect.js
@@ -3,7 +3,9 @@ import { MongoClient } from 'mongodb'
 // import { uri } from ""
 
 // const uri = 'mongodb://localhost:27017'
-import {uri} from "./credentials.js"
+// import {uri} from "./credentials.js"
+
+const uri = 'mongodb+srv://NatalliaPahosava:sladk11mo1@nataliapogosova.1b8tti3.mongodb.net/?retryWrites=true&w=majority'
 const client = new MongoClient(uri) // creating client mongo instance
 const db = client.db('bocacode-test') // connecting to db = bocacod-test
 
